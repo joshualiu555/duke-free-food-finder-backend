e1af38d8ab1bebf826d52a3e557e517932b732ed
Joshua Liu <liu.joshua555@gmail.com>
2026-06-11 23:41:32 -0500

    added testing only jwt secret

--- src/test/java/com/joshualiu/dukefreefoodfinderbackend/DukeFreeFoodFinderBackendApplicationTests.java
+++ src/test/java/com/joshualiu/dukefreefoodfinderbackend/DukeFreeFoodFinderBackendApplicationTests.java
@@ -9,7 +9,12 @@ import org.springframework.test.context.TestPropertySource;
         "spring.datasource.url=jdbc:h2:mem:testdb",
         "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        // only for testing
+        "jwt.secret=5pFaPwXnW8daLuHZOO9LGdoQxvHMGYcSNx09nZcENns",
+        "jwt.expiration=86400000",
+        "spring.mail.host=localhost",
+        "spring.mail.port=25"
 })
 class DukeFreeFoodFinderBackendApplicationTests {
 
