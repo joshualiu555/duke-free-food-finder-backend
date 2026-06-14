7666726702dbe001a6af15a864c5026f69e491dd
Joshua Liu <liu.joshua555@gmail.com>
2026-06-14 13:04:13 -0500

    fixed tests

--- src/test/java/com/joshualiu/dukefreefoodfinderbackend/DukeFreeFoodFinderBackendApplicationTests.java
+++ src/test/java/com/joshualiu/dukefreefoodfinderbackend/DukeFreeFoodFinderBackendApplicationTests.java
@@ -10,13 +10,11 @@ import org.springframework.test.context.TestPropertySource;
         "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.jpa.hibernate.ddl-auto=create-drop",
         "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-        "spring.mail.host=localhost",
-        "spring.mail.port=25",
-        // only for testing
-        "jwt.secret=5pFaPwXnW8daLuHZOO9LGdoQxvHMGYcSNx09nZcENns",
+        "jwt.secret=jwt-secret-for-testing",
         "jwt.expiration=86400000",
         "aws.s3.bucket=test-bucket",
-        "aws.s3.region=us-east-1"
+        "aws.s3.region=us-east-1",
+        "resend.api.key=resend-api-key-for-testing"
 
 })
 class DukeFreeFoodFinderBackendApplicationTests {
